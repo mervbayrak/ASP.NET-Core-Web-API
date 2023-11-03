8d2a044a98be06b13eb02a879a3f486323a3e1a3
mervbayrak <mervbayrak@hotmail.com>
2023-11-03 10:10:46 +0300

    Hateos kısmı yapılanlar

---
+++ bsStoreApp.Entities/LinkModels/LinkResourceBase.cs
@@ -0,0 +1,11 @@
+﻿namespace bsStoreApp.Entities.LinkModels
+{
+    public class LinkResourceBase
+    {
+        public LinkResourceBase()
+        {
+
+        }
+        public List<Link> Links { get; set; } = new List<Link>();
+    }
+}
