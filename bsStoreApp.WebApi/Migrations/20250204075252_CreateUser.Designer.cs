1ba642090f2bd324730f1084258c9ce40c538d0e
merve.bayrak <merve.bayrak@flo.com.tr>
2025-02-04 11:30:19 +0300

    sql server connections update

--- bsStoreApp.WebApi/Migrations/20250204075252_CreateUser.Designer.cs
+++ bsStoreApp.WebApi/Migrations/20250204075252_CreateUser.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using bsStoreApp.Repositories.EFCore;
 
@@ -11,9 +12,11 @@ using bsStoreApp.Repositories.EFCore;
 namespace bsStoreApp.WebApi.Migrations
 {
     [DbContext(typeof(RepositoryContext))]
-    partial class RepositoryContextModelSnapshot : ModelSnapshot
+    [Migration("20250204075252_CreateUser")]
+    partial class CreateUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
